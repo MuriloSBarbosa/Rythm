91b0297f4243722eea2009d8898ddee80d0b0fb8
MuriloSBarbosa <murilo.barbosa@sptech.school>
2022-11-22 00:15:03 -0300

    pages naipes

--- site/public/js/instrumentos.js
+++ site/public/js/instrumentos.js
@@ -27,16 +27,28 @@ for (let i = 0; i < imagens.length; i++) {
 
 function filtro(tipo) {
     var content = document.querySelectorAll(".content-instrumento");
+    var buttons = document.getElementsByName('button-filtro');
 
     for (let i = 0; i < content.length; i++) {
         const contentAtual = content[i];
+        const button = buttons[i];
+
+        button.classList = '';
         contentAtual.style.display = 'none';
     }
 
     if (tipo == 'cordas') {
+        btn_cordas.classList = 'active-filtro';
         content_cordas.style.display = 'flex';
-    } else {
+    } else if (tipo == 'madeiras') {
+        btn_madeiras.classList = 'active-filtro';
         content_madeiras.style.display = 'flex';
+    } else if (tipo == 'metais') {
+        btn_metais.classList = 'active-filtro';
+        content_metais.style.display = 'flex';
+    } else {
+        btn_percussao.classList = 'active-filtro';
+        content_percussao.style.display = 'flex';
     }
 }
 
