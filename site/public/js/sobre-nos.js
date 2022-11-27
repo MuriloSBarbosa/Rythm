150f3cc9f67454c34d8cb2fcef6598cc5bb743b4
MuriloSBarbosa <murilo.barbosa@sptech.school>
2022-11-27 19:30:33 -0300

    Finalizando galeria, scrool reveal e texto history

--- site/public/js/sobre-nos.js
+++ site/public/js/sobre-nos.js
@@ -1,14 +1,9 @@
 const controls = document.querySelectorAll('.control');
 
-let currentItem = 5;
+let currentItem = 7;
 
 const items = document.querySelectorAll('.item');
-
-items[5].scrollIntoView({
-    inline: "center",
-});
-
-window.scroll(0, 0);
+const descriptions = document.querySelectorAll('.description');
 
 const maxItems = items.length;
 
@@ -36,13 +31,46 @@ controls.forEach(control => {
             item.classList.remove('current-item')
         );
 
-        items[currentItem].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-            block: "nearest"
-        });
+        descriptions.forEach(description =>
+            description.classList.remove('activeDesc', 'activeDescLast'),
+        );
 
         items[currentItem].classList.add('current-item')
 
+        if (currentItem == maxItems - 1) {
+            items[currentItem].scrollIntoView({
+                inline: "end",
+                behavior: "smooth",
+                block: "nearest"
+            });
+            descriptions[currentItem].classList.add('activeDescLast');
+
+        } else {
+            items[currentItem].scrollIntoView({
+                inline: "center",
+                behavior: "smooth",
+                block: "nearest"
+            });
+            descriptions[currentItem].classList.add('activeDesc');
+        }
+
+
+
     })
 });
+
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.reveal', {
+    distance: "100px",
+    duration: 1000,
+    delay: 100
+});
+
+
+window.onload = function () {
+    items[7].scrollIntoView({
+        inline: "center",
+    });
+    window.scroll(0, 0);
+}
